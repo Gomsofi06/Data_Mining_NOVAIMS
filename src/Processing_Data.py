f83678465d384ebb9daf599eedcff560a99dbe10
Gomsofi06 <sofiagomes@MacBook-Air-de-Sofia-4.local>
2024-12-23 17:02:34 +0000

    some things

--- src/Processing_Data.py
+++ src/Processing_Data.py
@@ -22,7 +22,33 @@
 # ** **
 # 
 
-# In[37]:
+# ## <span style="color:salmon"> Notebook </span> 
+# 
+# In this notebook, after exploring our data and removing the duplicates in the other notebook, we are going to search for incoherencies, handle the missing values that can be imputed with simple techniques that do not tend to leak data, remove outliers and explore the results of our preprocessing.
+
+# ## <span style="color:salmon"> Table of Contents </span>
+# 
+# <a class="anchor" id="top"></a>
+# 
+# 1. [Import Libraries](#one-bullet) <br>
+# 
+# 2. [Import Datasets](#two-bullet) <br>
+# 
+# 3. [Data Types](#three-bullet) <br>
+# 
+# 4. [Incoherencies](#four-bullet) <br>
+# 
+# 5. [Handling Missing Values](#five-bullet) <br>
+# 
+# 6. [Removing Outliers  & Changing Distributions](#six-bullet) <br>
+# 
+# 7. [Export Datasets](#seven-bullet) <br> 
+# 
+
+# <a class="anchor" id="one-bullet"></a>
+# ## <span style="color:salmon"> 1. Import Libraries </span> 
+
+# In[1]:
 
 
 import pandas as pd 
@@ -35,6 +61,12 @@ from scipy.stats import chi2_contingency
 import scipy.stats as stats
 import warnings
 
+from utils import *
+import warnings
+
+# Suppress specific warnings
+warnings.filterwarnings("ignore", category=pd.errors.DtypeWarning)
+
 from math import ceil
 from sklearn.impute import KNNImputer
 
@@ -42,144 +74,126 @@ pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
 
 
-# In[38]:
+# <a class="anchor" id="two-bullet"> 
+# 
+# ## <span style="color:salmon"> 2. Import Dataset </span> 
+# 
+# <a href="#top">Top &#129033;</a>
+
+# In[2]:
 
 
 df = pd.read_csv("../dataset/df_explore.csv")
 
 
-# ## <span style="color:salmon">2. Pre-processing Data </span> 
+# <a class="anchor" id="three-bullet"> 
+# 
+# ## <span style="color:salmon">3. Data types </span> 
+# 
+# <a href="#top">Top &#129033;</a>
 
-# #### <span style="color:salmon">2.1 Features</span> 
-
-# First, we need to divide the columns that have metric features:
-
-# In[39]:
-
-
-non_metric_features = ["customer_region", "last_promo", "payment_method"]
-metric_features = df.columns.drop(non_metric_features)
-metric_features
+# In[3]:
 
 
-# ##### <span style="color:salmon"> 2.1.1 Numerical features </span>
-
-# To see better the distribution of metric features, we need to create histograms:
-
-# In[40]:
-
-
-sns.set()
-
-# Set up the figure and axes
-rows, cols = 12, 5  
-fig, axes = plt.subplots(rows, cols, figsize=(25, 30))  
-
-# Plot each feature
-for ax, feat in zip(axes.flatten(), metric_features):
-    ax.hist(df[feat], bins=20, color='skyblue', edgecolor='black')  
-    ax.set_title(feat, fontsize=10, y=-0.2)  
-    
-# Hide any unused subplots if the number of features is less than rows * cols
-for ax in axes.flatten()[len(metric_features):]:
-    ax.set_visible(False)
-
-# Set a global title and adjust layout
-plt.suptitle("Numeric Variables' Histograms", fontsize=16, y=1.02)  
-plt.tight_layout()
-plt.show()
+numerical_features = ['customer_age', 'vendor_count', 'product_count', 'is_chain',
+                      'first_order', 'last_order', 'CUI_American', 'CUI_Asian',
+                      'CUI_Beverages', 'CUI_Cafe', 'CUI_Chicken Dishes', 'CUI_Chinese',
+                      'CUI_Desserts', 'CUI_Healthy', 'CUI_Indian', 'CUI_Italian',
+                      'CUI_Japanese', 'CUI_Noodle Dishes', 'CUI_OTHER',
+                      'CUI_Street Food / Snacks', 'CUI_Thai', 'DOW_0', 'DOW_1', 'DOW_2',
+                      'DOW_3', 'DOW_4', 'DOW_5', 'DOW_6', 'HR_0', 'HR_1', 'HR_2', 'HR_3',
+                      'HR_4', 'HR_5', 'HR_6', 'HR_7', 'HR_8', 'HR_9', 'HR_10', 'HR_11',
+                      'HR_12', 'HR_13', 'HR_14', 'HR_15', 'HR_16', 'HR_17', 'HR_18', 'HR_19',
+                      'HR_20', 'HR_21', 'HR_22', 'HR_23'
+                     ]
 
 
-# Plotting variables excluding zero, because it biases the scaling: (used to define outliers threshold)
-
-# In[41]:
+# In[4]:
 
 
-CUI_variables = ["CUI_American", "CUI_Asian", "CUI_Beverages", "CUI_Cafe", 
-                 "CUI_Chicken Dishes", "CUI_Chinese", "CUI_Desserts", 
-                 "CUI_Healthy", "CUI_Indian", "CUI_Italian", "CUI_Japanese", 
-                 "CUI_Noodle Dishes", "CUI_OTHER", "CUI_Street Food / Snacks", 
-                 "CUI_Thai", "DOW_0" ,"DOW_1", "DOW_2", "DOW_3", "DOW_4", "DOW_5", "DOW_6",
-                 "HR_1", "HR_2", "HR_3", "HR_4", "HR_5", "HR_6", "HR_7", "HR_8", "HR_9", 
-                 "HR_10", "HR_11", "HR_12", "HR_13", "HR_14", "HR_15", "HR_16", "HR_17", "HR_18", 
-                 "HR_19", "HR_20", "HR_21", "HR_22", "HR_23"]
-
-sns.set()
-
-# Calculate rows and columns
-n_features = len(CUI_variables)
-cols = 5  #
-rows = -(-n_features // cols)  
-
-# Set up the figure and axes
-fig, axes = plt.subplots(rows, cols, figsize=(25, rows * 5)) 
-
-# Plot each CUI variable
-for ax, feat in zip(axes.flatten(), CUI_variables):
-    data_no_zero = df[df[feat] != 0][feat] # Exclude zero values
-    ax.hist(data_no_zero, bins=20, color='skyblue', edgecolor='black')  
-    ax.set_title(feat, fontsize=10, y=-0.2)  
-
-# Hide  unused subplots if the number of CUI variables is less than rows * cols
-for ax in axes.flatten()[len(CUI_variables):]:
-    ax.set_visible(False)
-
-# Set a global title and adjust layout
-plt.suptitle("CUI Variables' Histograms (Excluding Zero)", fontsize=16, y=1.02)  
-plt.tight_layout()
-plt.show()
+numerical_df = df[numerical_features]
 
 
-# ##### <span style="color:salmon"> 2.1.2 Categorical features </span>
+# As all necessary analysis and explanations were given in the previous notebook, this notebook will only have the necessary changes on the data types.
 
-# Now, we do the same for the non-metric features:
-
-# In[42]:
+# In[5]:
 
 
-for column in non_metric_features:
-    
-    categories = df[column].value_counts()
-
-    categories_sorted = categories.sort_values(ascending=True)
-
-    data_filtered = df[df[column].isin(categories_sorted.index)]
-    
-   
-    plt.figure(figsize=(10, 5))
-    sns.countplot(data=data_filtered, 
-                  x=column, 
-                  order=categories_sorted.index,  
-                  palette='tab20b')
-    
-  
-    plt.xlabel(column, fontsize=14)
-    plt.ylabel('Count', fontsize=14)
-    plt.title(f'Categories in {column}')
-    
-    
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.show()
+float_to_int(df, ['customer_age', 'first_order', 'HR_0'])
 
 
-# #### <span style="color:salmon"> 2.2 Missing Values </span> 
+# <a class="anchor" id="four-bullet"> 
+# 
+# ## <span style="color:salmon"> 4. Incoherencies</span> 
+# 
+# <a href="#top">Top &#129033;</a>
+
+# ## <span style="color:yellow"> O QUE FAZER COM AS INCOERENCIAS?</span> 
+# 
+# ## <span style="color:yellow"> CHECK FOR MORE INCOERENCIAS</span> 
+
+# 1. The mean of product_count is 5 and exists one number with value 269
+
+# In[6]:
+
+
+df.loc[(df["product_count"]==269)]
+
+
+# 2. the column HR_0 is the only with 0 requests
+
+# In[7]:
+
+
+df['HR_0'].max()
+
+
+# 3. Product_count == 0 and vendor_count >= 1:
+
+# In[8]:
+
+
+df.loc[(df["product_count"]==0) & (df["vendor_count"]>=1)]
+
+
+# Replace product_count = 0 with NaN for these rows:
+
+# In[9]:
+
+
+df.loc[(df["product_count"] == 0) & (df["vendor_count"] >= 1), "product_count"] = np.nan
+
+
+# In[10]:
+
+
+product_vendor_count_missing = df.loc[(df["product_count"].isna()) & (df["vendor_count"] >= 1)]
+product_vendor_count_missing
+
+
+# <a class="anchor" id="five-bullet"> 
+# 
+# ## <span style="color:salmon"> 5. Handling Missing Values</span> 
+# 
+# <a href="#top">Top &#129033;</a>
 
 # Dealing with missing values effectively is crucial to ensure our dataset's integrity and the accuracy of your analysis. 
+# 
+# So, first we check the percentage of missing values:
 
-# In[43]:
+# In[11]:
 
 
-# Check percentage of missing values:
 missing_percentage = ((df.isnull().sum() / len(df)) * 100).sort_values(ascending=False)
-missing_percentage = missing_percentage[missing_percentage > 0]
 
-print(f"Percentage of missing values:\n {missing_percentage}")
+missing_percentage[missing_percentage > 0].sort_values(ascending=False)
+
+print(f"Percentage of missing values:\n{missing_percentage}")
 
 
-# In case of numerical features, the strategie we used to deal with missing values is input with the median:
+# 1. In case of numerical features, the strategie we used to deal with missing values is input with the median:
 
-# In[44]:
+# In[12]:
 
 
 # Input missing values in numerical features using median:
@@ -189,47 +203,18 @@ for column in median_variables:
     df[column] = df[column].fillna(median_value)
 
 
-# In[45]:
+# In[13]:
 
 
-# Check percentage of missing values:
 missing_percentage = ((df.isnull().sum() / len(df)) * 100).sort_values(ascending=False)
 missing_percentage = missing_percentage[missing_percentage > 0]
 
 print(f"Percentage of missing values:\n {missing_percentage}")
-
-
-# In case of categorical features, the strategie we used to deal with missing values is input with the mode:
-
-# In[46]:
-
-
-# Input missing values in categorical features using mode:
-mode_variables = ["last_promo", "customer_region"]
-for column in mode_variables:
-    mode_value = df[column].mode()[0]
-    df[column] = df[column].fillna(mode_value)
-
-
-# In[47]:
-
-
-# Check percentage of missing values:
-missing_percentage = ((df.isnull().sum() / len(df)) * 100).sort_values(ascending=False)
-missing_percentage = missing_percentage[missing_percentage > 0]
-
-print(f"Percentage of missing values:\n {missing_percentage}")
-
-
-# In[48]:
-
-
-missing_percentage_first_order = df.loc[df["first_order"].isna()]
 
 
 # In order to threat the missing values in first_order, we use the technique KNNImputer, which for each point with missing values, finds the K nearest neighbors based on a distance metric and replaces the missing value with the average value of the K nearest neighbors. 
 
-# In[49]:
+# In[14]:
 
 
 # Input missing values in first_order using knn:
@@ -253,72 +238,65 @@ df.loc[df["first_order"].isna(), "first_order"] = imputed_values[:, 0]
 print("Missing values in first_order have been imputed using KNN.")
 
 
-# Check if missing values have been imputed for first_order:
-
-# In[50]:
-
-
-df.loc[missing_percentage_first_order.index]
-
-
-# #### <span style="color:salmon"> 2.3 Strange values </span> 
-
-# Check the stange values Product_count == 0 and vendor_count >= 1:
-
-# In[51]:
-
-
-df.loc[(df["product_count"]==0) & (df["vendor_count"]>=1)]
-
-
-# Replace product_count = 0 with NaN for these rows:
-
-# In[52]:
-
-
-df.loc[(df["product_count"] == 0) & (df["vendor_count"] >= 1), "product_count"] = np.nan
-
-
-# In[53]:
-
-
-product_vendor_count_missing = df.loc[(df["product_count"].isna()) & (df["vendor_count"] >= 1)]
-product_vendor_count_missing
-
-
 # Input missing values in product_count using KNNImputer:
 
-# In[54]:
+# In[15]:
 
 
+# Select rows where "product_count" is NaN
 knn_rows = df.loc[df["product_count"].isna()]
 
-features_for_imputation = metric_features.drop(["product_count", "customer_age", "is_chain", "CUI_American", "CUI_Asian", "CUI_Beverages", "CUI_Cafe", 
-                                                 "CUI_Chicken Dishes", "CUI_Chinese", "CUI_Desserts", "CUI_Healthy", "CUI_Indian", "CUI_Italian", "CUI_Japanese", 
-                                                 "CUI_Noodle Dishes", "CUI_OTHER", "CUI_Street Food / Snacks", "CUI_Thai"])
+# Drop the unnecessary columns from numerical_df
+features_for_imputation = numerical_df.drop(
+    columns=[
+        "product_count", "customer_age", "is_chain", "CUI_American", "CUI_Asian", 
+        "CUI_Beverages", "CUI_Cafe", "CUI_Chicken Dishes", "CUI_Chinese", "CUI_Desserts", 
+        "CUI_Healthy", "CUI_Indian", "CUI_Italian", "CUI_Japanese", "CUI_Noodle Dishes", 
+        "CUI_OTHER", "CUI_Street Food / Snacks", "CUI_Thai"
+    ]
+)
 
-imputation_data = df.loc[df["product_count"].isna(), features_for_imputation]
+# Extract the rows for imputation
+imputation_data = df.loc[df["product_count"].isna(), features_for_imputation.columns]
 
+# Perform KNN imputation
 knn_imputer = KNNImputer(n_neighbors=5, weights="uniform")
-
-
-
-
 imputed_values = knn_imputer.fit_transform(imputation_data)
 
-df.loc[df["product_count"].isna(), "product_count"] = imputed_values[:, 0]  
+# Assign the imputed values back to the DataFrame
+df.loc[df["product_count"].isna(), "product_count"] = imputed_values[:, 0]
 
 print("Missing values in product_count have been imputed using KNN.")
 
 
-# In[55]:
+# 2. In case of categorical features, the strategie we used to deal with missing values is input with the mode:
+
+# In[16]:
 
 
-# Check if missing values have been imputed for product_count:
-df.loc[product_vendor_count_missing.index]
+# Input missing values in categorical features using mode:
+mode_variables = ["last_promo", "customer_region"]
+for column in mode_variables:
+    mode_value = df[column].mode()[0]
+    df[column] = df[column].fillna(mode_value)
 
 
-# #### <span style="color:salmon">2.4 Outliers </span> 
+# To check if every missing value, has been taken.
+
+# In[17]:
+
+
+missing_percentage = ((df.isnull().sum() / len(df)) * 100).sort_values(ascending=False)
+missing_percentage = missing_percentage[missing_percentage > 0]
+
+print(f"Percentage of missing values:\n {missing_percentage}")
+
+
+# <a class="anchor" id="six-bullet"> 
+# 
+# ## <span style="color:salmon"> 6. Removing Outliers</span> 
+# 
+# <a href="#top">Top &#129033;</a>
 
 # Outliers are data points that deviate significantly from the rest of the observations in a dataset.
 # 
@@ -326,24 +304,24 @@ df.loc[product_vendor_count_missing.index]
 # 
 # So, we have to threat them.
 
-# ##### <span style="color:salmon">2.4.1 Outliers visualization </span> 
+# 1. Outliers visualization
 
-# In[56]:
+# In[18]:
 
 
 sns.set()
 
 # Set up the figure and axes
-rows, cols = 12, 5  
+rows, cols = 13, 4  
 fig, axes = plt.subplots(rows, cols, figsize=(25, 30))  
 
 # Plot each feature as a box plot
-for ax, feat in zip(axes.flatten(), metric_features):
+for ax, feat in zip(axes.flatten(), numerical_df):
     sns.boxplot(data=df, x=feat, ax=ax, color='skyblue')  
     ax.set_title(feat, fontsize=10, y=-0.2)  #
 
 # Hide unused subplots:
-for ax in axes.flatten()[len(metric_features):]:
+for ax in axes.flatten()[len(numerical_df):]:
     ax.set_visible(False)
 
 # Set a global title and adjust layout
@@ -352,35 +330,37 @@ plt.tight_layout()
 plt.show()
 
 
-# ##### <span style="color:salmon">2.4.2 Outliers removal </span> 
+# ## <span style="color:yellow"> TRATAR OUTLIERS INDIVIDUALMENTE</span> 
+
+# 2. Outliers removal
 
 # There is two methods to treat with outliers:
-# 1. Automatic method 
-# 2. Manual method
+# - Automatic method 
+# - Manual method
 # 
 # We will try those two and compare which is better in our database.
 
-# 1. Using the Automatic method:
+# - Using the Automatic method:
 
-# In[57]:
+# In[19]:
 
 
 # Compute the inter-quartile range
-q1 = df[metric_features].quantile(0.25)
-q3 = df[metric_features].quantile(0.75)
+q1 = df[numerical_features].quantile(0.25)
+q3 = df[numerical_features].quantile(0.75)
 iqr = q3 - q1
 
 # Compute the limits:
 lower_lim = q1 - (1.5 * iqr)
 upper_lim = q3 + (1.5 * iqr)
 
-for feature in metric_features:
+for feature in numerical_features:
     print(f"{feature:<25}  Lower Limit: {lower_lim[feature]:>10}      Upper Limit: {upper_lim[feature]:>10}")
 
 
 # Observations in which all features are outliers:
 
-# In[58]:
+# In[20]:
 
 
 def identify_outliers(dataframe, metric_features, lower_lim, upper_lim):
@@ -434,17 +414,18 @@ def identify_outliers(dataframe, metric_features, lower_lim, upper_lim):
     return outliers, obvious_outliers
     
     
-outliers, obvious_outliers = identify_outliers(df, metric_features, lower_lim, upper_lim)
+outliers, obvious_outliers = identify_outliers(df, numerical_features, lower_lim, upper_lim)
 
 
 # Conclusion: There is no observation in which all features are outliers. Since there is no outlier in 'HR_0', 'last_order', 'first_order'.
 # 
 # Check if there is any observation only with outliers, except on these features.
 
-# In[59]:
+# In[21]:
 
 
-metric_features_test = metric_features.drop(['HR_0', 'last_order', 'first_order'])
+metric_features_test = numerical_df.drop(columns=['HR_0', 'last_order', 'first_order'])
+
 outliers, obvious_outliers = identify_outliers(df, metric_features_test, lower_lim, upper_lim)
 
 
@@ -452,11 +433,11 @@ outliers, obvious_outliers = identify_outliers(df, metric_features_test, lower_l
 
 # Observations in which at least one feature is an outlier:
 
-# In[60]:
+# In[22]:
 
 
 filters_iqr = []                                            
-for metric in metric_features:
+for metric in numerical_features:
     llim = lower_lim[metric]
     ulim = upper_lim[metric]
     filters_iqr.append(df[metric].between(llim, ulim, inclusive='neither'))
@@ -464,13 +445,13 @@ for metric in metric_features:
 filters_iqr_all = pd.concat(filters_iqr, axis=1).all(axis=1)
 
 
-# In[61]:
+# In[23]:
 
 
 filters_iqr_all
 
 
-# In[62]:
+# In[24]:
 
 
 len(df[~filters_iqr_all])
@@ -483,9 +464,9 @@ print(f"Percentage of data kept after removing outliers: {percentage_data_kept}%
 
 # Conclusion: All observations have outliers in some feature
 
-# 2. Using the manual method:
+# - Using the manual method:
 
-# In[63]:
+# In[25]:
 
 
 filters_manual1 = (
@@ -591,13 +572,13 @@ filters_manual1 = (
 df_out_man1 = df[filters_manual1]
 
 
-# In[64]:
+# In[26]:
 
 
 print('Percentage of data kept after removing outliers:', 100*(np.round(df_out_man1.shape[0] / df.shape[0], decimals=5)))
 
 
-# In[65]:
+# In[27]:
 
 
 filters_manual2 = (
@@ -703,13 +684,13 @@ filters_manual2 = (
 df_out_man2 = df[filters_manual2]
 
 
-# In[66]:
+# In[28]:
 
 
 print('Percentage of data kept after removing outliers:', 100*(np.round(df_out_man2.shape[0] / df.shape[0], decimals=5)))
 
 
-# In[67]:
+# In[29]:
 
 
 filters_manual3 = (
@@ -815,7 +796,7 @@ filters_manual3 = (
 df_out_man3 = df[filters_manual3]
 
 
-# In[68]:
+# In[30]:
 
 
 # Number of observations with at least one features considered an outlier
@@ -827,13 +808,43 @@ print(f"Percentage of data kept after removing outliers: {percentage_data_kept_m
 
 # We conclude that we should see what is common to both methods and remove only that because the automatic method is removing a very high percentage of the data.
 
-# In[69]:
+# In[31]:
 
 
 df = df[(filters_iqr_all | filters_manual3)]
 
 
-# In[70]:
+# In[32]:
+
+
+sns.set()
+
+# Set up the figure and axes
+rows, cols = 13, 4  
+fig, axes = plt.subplots(rows, cols, figsize=(25, 30))  
+
+# Plot each feature as a box plot
+for ax, feat in zip(axes.flatten(), numerical_df):
+    sns.boxplot(data=df, x=feat, ax=ax, color='skyblue')  
+    ax.set_title(feat, fontsize=10, y=-0.2)  #
+
+# Hide unused subplots:
+for ax in axes.flatten()[len(numerical_df):]:
+    ax.set_visible(False)
+
+# Set a global title and adjust layout
+plt.suptitle("Numeric Variables' Box Plots", fontsize=16, y=1.02)  
+plt.tight_layout()
+plt.show()
+
+
+# <a class="anchor" id="seven-bullet"> 
+# 
+# ## <span style="color:salmon"> 7. Export Datasets</span> 
+# 
+# <a href="#top">Top &#129033;</a>
+
+# In[33]:
 
 
 # Store in df_preprocessing the DataFrame of our dataset df
